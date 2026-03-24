b9208931750947945955180fa0f01a0e02bb9701
Perd0sh <itsggtewv@gmail.com>
2026-03-24 08:31:06 +0300

    подключил 2 стандартных библиотеки C++,сделал тип окна без рамки, добавил пока что еще фигуру в центр экрана, сделал спавн этой фигуры в центре,

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Game", sf::Style::None);
+    sf::CircleShape player(40.f); // создаю персонажа 
+    player.setFillColor(sf::Color::Red); // задаю цвет
+    player.setOrigin({ 40.f, 40.f });   
+    player.setPosition({ 960, 540 }); // спавн в центре
 
-    while (window.isOpen())
+        while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+
+            while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+window.clear();
+window.draw(player);
+window.display();
     }
 }
